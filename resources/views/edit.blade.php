e5129eaaad713667c553d5d25fc80843a5744bd4
aldinpramudya <aldinarielpramudya@gmail.com>
2023-05-07 20:06:53 +0700

    Pratikum 2

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <form action="/articles/{{$article->id}}" method="post" enctype="multipart/form-data">
+        @method('PUT')
         @csrf
         <div class="form-group">
             <label for="title">Judul</label>
@@ -15,8 +16,9 @@
         <div class="form-group">
             <label for="image">Feature Image</label>
             <input type="file" class="form-control" required="required" name="image" value="{{$article->featured_image}}"><br>
-            <img width="150px" src="{{asset('asset('storage/'.$article->featured_image)}}">
+            <img width= "150px" src="{{asset('storage/'.$article->featured_image)}}">
         </div>
+        <button type="submit" class="btn btn-primary float-right">Ubah Data</button>
     </form>
 </div>
 @endsection
